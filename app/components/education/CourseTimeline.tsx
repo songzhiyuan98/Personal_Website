0cc9b692ece9ab9d40411cafed7e2628e25c78d1
Zhiyuan <songzhiyuan98@gmail.com>
2024-12-31 21:14:10 -0800

    delete unused import in CourseTimeline

--- app/components/education/CourseTimeline.tsx
+++ app/components/education/CourseTimeline.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import AnimatedText from "../common/AnimatedText"
-import { motion, useScroll, useTransform } from "framer-motion"
+import { motion } from "framer-motion"
 
 interface CourseItem {
   date: string
