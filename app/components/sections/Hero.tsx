757bdbdca764b29151ca26631849bc3603f0b9f3
Zhiyuan <songzhiyuan98@gmail.com>
2025-01-03 21:30:28 -0800

    修复github图标显示错误

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -36,13 +36,13 @@ export default function Hero() {
           className="flex gap-4 items-center flex-col sm:flex-row"
         >
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://github.com/songzhiyuan98"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              className="invert dark:invert-0 mr-2"
               src="/github.svg"
               alt="GitHub"
               width={20}
